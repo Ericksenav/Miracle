ea145ccd08627429cae233a65bcedc73e04cc5aa
Ericksenav <52509655+Ericksenav@users.noreply.github.com>
2024-08-16 00:19:37 -0500

    pagina miracle

---
+++ src/utils/data.ts
@@ -0,0 +1,34 @@
+const services = [
+    {
+        title: "Branding",
+        description: "Diseñamos tu marca desde cero mediante un concepto e identidad sólida. Te ayudamos con todo desde el nombre, diseño de logo e imagen, giro y estructura de negocio",
+        icon: "images/Servicios/ICONO BRANDING.png"
+    },
+    {
+        title: "Redes Sociales",
+        description: "Desarrollamos estrategias dinámicas y cautivadoras en redes sociales para aumentar el compromiso, impulsar el crecimiento de seguidores y fortalecer la comunidad en línea de tu marca.",
+        icon: "images/Servicios/ICONO REDES SOCIALES DEGRADADO.png"
+    },
+    {
+        title: "Diseño Web",
+        description: "Diseñamos y desarrollamos sitios web atractivos, funcionales y optimizados para ofrecer una experiencia de usuario excepcional y convertir visitantes en clientes.",
+        icon: "images/Servicios/ICONOS WEB.png"
+    },
+    {
+        title: "Diseño Gráfico",
+        description: "Desarrollamos estrategias creativas e innovadoras, creando valor a travéz de piezas gráficas.",
+        icon: "images/Servicios/ICONOS Diseño Grafico.png"
+    },
+    {
+        title: "Audiovisual",
+        description: "Creamos contenido audiovisual impactante y cautivador, desde videos promocionales hasta animaciones y presentaciones, para contar la historia de tu marca de manera visualmente atractiva.",
+        icon: "images/Servicios/ICONOS AUDIOVISUAL DEGRADADO.png"
+    },
+    {
+        title: "Marketing de Influencers",
+        description: "Colaboramos con influencers relevantes para amplificar la visibilidad de tu marca, generar confianza y credibilidad entre tu audiencia y aumentar el compromiso y las conversiones.",
+        icon: "images/Servicios/ICONO Marketing de Influencer.png"
+    },
+];
+
+export { services };
